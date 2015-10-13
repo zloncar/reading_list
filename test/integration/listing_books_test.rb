cad60449b4f202be9e3a1876fc3c916603f6a08e
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2015-10-13 14:32:21 +0100

    Add books listing.

---
+++ test/integration/listing_books_test.rb
@@ -0,0 +1,16 @@
+require_relative '../test_helper.rb'
+
+class ListingBooksTest < ActionDispatch::IntegrationTest
+  setup do
+    Book.create!(title: 'Pragmatic Programmer', rating: 5)
+    Book.create!(title: "Ender's Game", rating: 5)
+  end
+
+  test 'listing books' do
+    get '/books'
+
+    assert_equal Mime::JSON, response.content_type
+    assert_equal 200, response.status
+    assert_equal Book.count, JSON.parse(response.body).size
+  end
+end
