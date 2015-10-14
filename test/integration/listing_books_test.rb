10c8927166126fbb16266a0a571e5b865350334f
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2015-10-14 13:26:44 +0100

    Extract json helper method.

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -11,7 +11,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
 
     assert_equal Mime::JSON, response.content_type
     assert_equal 200, response.status
-    assert_equal Book.count, JSON.parse(response.body).size
+    assert_equal Book.count, json(response.body).size
   end
 
   test 'listing top rated books' do
@@ -19,6 +19,6 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
 
     assert_equal Mime::JSON, response.content_type
     assert_equal 200, response.status
-    assert_equal 1, JSON.parse(response.body).size
+    assert_equal 1, json(response.body).size
   end
 end
