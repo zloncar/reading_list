489569304a48dcb5311ecd3fc8bc9007842a975f
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2015-10-15 19:42:57 +0100

    Create finished scope and add asserts to spec.

--- test/integration/listing_finished_books_test.rb
+++ test/integration/listing_finished_books_test.rb
@@ -1,11 +1,19 @@
 require_relative '../test_helper'
 
 class ListingFinishedBooksTest < ActionDispatch::IntegrationTest
+
+  setup do
+    Book.create!(title: 'Finished', finished_at: 1.day.ago)
+    Book.create!(title: 'Not Finished', finished_at: nil)
+  end
+
   test 'list finished books in JSON' do
     get '/finished_books', {}, { 'Accept' => 'application/json' }
 
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
+
+    assert_equal 1, json(response.body).size
   end
 
   test 'list finished books in XML' do
@@ -13,5 +21,7 @@ class ListingFinishedBooksTest < ActionDispatch::IntegrationTest
 
     assert_equal 200, response.status
     assert_equal Mime::XML, response.content_type
+
+    assert_equal 1, Hash.from_xml(response.body)['books'].size
   end
 end
